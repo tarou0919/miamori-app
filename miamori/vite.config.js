329c32551ab48f9e2aad423f5bc01983cc87a081
りょうま <ryoma25zakinishi@gmail.com>
2026-04-18 11:19:53 +0900

    🔧 base pathを修正

--- miamori/vite.config.js
+++ miamori/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig({ base: '/miamori-app/',
   plugins: [react()],
 })
